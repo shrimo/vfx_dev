7db1b7f869672d1c80cdd0efdd758e455977c937
Victor Lavrentev <victor.lavrentev@gmail.com>
2020-01-21 16:51:58 +0200

    Add existing file

--- maya/general_lesson/lesson_v02.py
+++ maya/general_lesson/lesson_v02.py
@@ -15,6 +15,7 @@ lesson_v02.start()
 
 import maya.cmds as cmds
 import math
+import random
 
 
 def set_key_frame(shape, a, b, c):
@@ -40,10 +41,26 @@ def create_sin_boxes(group_name, shape_name, a, b, c):
 
     return True
 
+def create_sphere_boxes(group_name, shape_name, r):
+    cmds.group(em=True, name=group_name)
+    for amount in xrange(1, 300):
+        p_name = shape_name + '_' + str(amount)
+        theta = random.uniform(0, 360)
+        phi = 90 * (1 - math.sqrt(random.uniform(0, 1)))
+        x_path = r * math.cos(theta)*math.sin(phi)
+        y_path = r * math.sin(theta)*math.sin(phi)
+        z_path = r * math.cos(phi)
+        cmds.polyCube(n=p_name)  # create polyCube name by p_ + amount
+        cmds.setAttr(p_name+'.translate', x_path, y_path, z_path)
+        cmds.group(p_name, parent=group_name)
+
+    return True
+
 
 def start():
     # shapes = cmds.ls(selection=True, shapes=True, dagObjects=True)
-    m_name = 'motion_01'
-    cmds.polyCube(n=m_name)
-    print set_key_frame(m_name, 10, 0.3, 1)
+    # m_name = 'motion_01'
+    # cmds.polyCube(n=m_name)
+    # print set_key_frame(m_name, 10, 0.3, 1)
     # create_sin_boxes('boxes', 'sin', 10, 0.3, 1)
+    create_sphere_boxes('boxes', 'sphere', 10)
