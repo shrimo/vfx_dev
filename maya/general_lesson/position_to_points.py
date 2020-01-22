ee5324c8c47df8662833e5f25bcafdb61f307e74
Victor Lavrentev <victor.lavrentev@gmail.com>
2020-01-22 18:09:54 +0200

    Add existing file

--- maya/general_lesson/position_to_points.py
+++ maya/general_lesson/position_to_points.py
@@ -12,22 +12,21 @@ def read_exr(filename, group_name, shape_name, scale):
     exr_file = OpenEXR.InputFile(filename)
 
     # read RGB channel
-    all_pos = []
+    pos = []
     start_get_time = time.time()
     for c in 'RGB':
         position_str = exr_file.channel(c, pt)
         position = array.array('f', position_str).tolist()
-        all_pos.append([val * scale for val in position if val != 0.0 in position])
+        pos.append([val * scale for val in position if val != 0.0 in position])
     end_get_time = time.time()
 
     # create object
     cmds.group(em=True, name=group_name)
     start_create_time = time.time()
-    for i in range(len(all_pos[0])):
+    for i in range(len(pos[0])):
         p_name = shape_name + '_' + str(i)
         cmds.polyCube(n=p_name)
-        cmds.setAttr(p_name + '.translate',
-                     all_pos[0][i], all_pos[1][i], all_pos[2][i])
+        cmds.setAttr(p_name + '.translate', pos[0][i], pos[1][i], pos[2][i])
         cmds.group(p_name, parent=group_name)
     end_create_time = time.time()
 
