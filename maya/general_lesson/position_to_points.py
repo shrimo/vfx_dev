1154f6d68dfc714788f4ad881b667f2734f86f42
Victor Lavrentev <victor.lavrentev@gmail.com>
2020-01-22 16:34:44 +0200

    Add existing file

--- maya/general_lesson/position_to_points.py
+++ maya/general_lesson/position_to_points.py
@@ -4,39 +4,42 @@ import OpenEXR
 import amg_imath
 import array
 import maya.cmds as cmds
+import time
 
 
-def read_exr(filename, group_name, shape_name):
+def read_exr(filename, group_name, shape_name, scale):
     pt = amg_imath.PixelType(amg_imath.PixelType.FLOAT)
     exr_file = OpenEXR.InputFile(filename)
-    dw = exr_file.header()['dataWindow']
-    size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
     cmds.group(em=True, name=group_name)
 
     # read RGB channel
-    pos_x = []
-    pos_y = []
-    pos_z = []
-    for c in 'RGB':
+    all_pos = []
+
+    start_get_time = time.time()
+    for i, c in enumerate('RGB'):
         position_str = exr_file.channel(c, pt)
         position = array.array('f', position_str).tolist()
-        for pos in position:
-            if pos != 0.0:
-                if c == 'R':
-                    pos_x.append(pos * 10)
-                if c == 'G':
-                    pos_y.append(pos * 10)
-                if c == 'B':
-                    pos_z.append(pos * 10)
+        pos = []
+        for p in position:
+            if p != 0.0:
+                pos.append(p * scale)
+        all_pos.append(pos)
+    end_get_time = time.time()
 
     # create object
-    for i in range(len(pos_x)):
+    start_create_time = time.time()
+    for i in range(len(all_pos[0])):
         p_name = shape_name + '_' + str(i)
-        cmds.polyCube(n=p_name)  # create polyCube name by p_ + i
-        cmds.setAttr(p_name + '.translate', pos_x[i], pos_y[i], pos_z[i])
+        cmds.polyCube(n=p_name)
+        cmds.setAttr(p_name + '.translate',
+                     all_pos[0][i], all_pos[1][i], all_pos[2][i])
         cmds.group(p_name, parent=group_name)
+    end_create_time = time.time()
+
+    print"--- {} P time ---".format(end_get_time - start_get_time)
+    print"--- {} Create time ---".format(end_create_time - start_create_time)
 
 
 def start():
-    exr_file = '//home/v.lavrentev/project/class/vfx_dev/openexr/position.exr'
-    read_exr(exr_file, 'boxes', 'sphere')
+    exr_file='//home/v.lavrentev/project/class/vfx_dev/openexr/position.exr'
+    read_exr(exr_file, 'boxes', 'sphere', 10)
