8fa9387fe1a14b6f68166b797b7221be8126313c
Victor Lavrentev <victor.lavrentev@gmail.com>
2020-02-11 15:43:12 +0200

    progress bar
    add progress bar

--- nuke/3dequalizer_to_nuke_01.py
+++ nuke/3dequalizer_to_nuke_01.py
@@ -1,4 +1,6 @@
 # read from 3dequalizer track
+import threading
+
 
 def create_track():
 	# create transfrom node and set animation
@@ -7,19 +9,27 @@ def create_track():
     k.setAnimated()
     return k, t_node
 
-filepath = '/home/v.lavrentev/project/class/eq_to_nuke/for_python_min.txt'
-with open(filepath, "r") as fp:
-	tracks_count = int(fp.readline().split('\n')[0]) # read first line in file. tracks count
-	print tracks_count
-	for block in range(tracks_count):
-		track_namber = int(fp.readline().split('\n')[0]) # tracks number
-		offset = int(fp.readline().split('\n')[0]) # frame offset
-		frame_range = int(fp.readline().split('\n')[0]) # frame range (time)
-		k, t_node = create_track() # create transform node for track
-		for frame in range(frame_range):
-			track = fp.readline().split(' ') # read track data number track, X, Y
-			X = float(track[1]) - t_node['center'].getValue()[0] # move X by cetner
-            Y = float(track[2]) - t_node['center'].getValue()[1] # move Y by cetner
-            k.setValueAt(X, int(track[0]), 0) # set keyframe for X
-            k.setValueAt(Y, int(track[0]), 1) # set keyframe for Y
+def get_track_data():
+	task = nuke.ProgressTask('Read track data...')
+	filepath = '/home/v.lavrentev/project/class/eq_to_nuke/for_python_min.txt'
+	with open(filepath, "r") as fp:
+		tracks_count = int(fp.readline().split('\n')[0]) # read first line in file. tracks count
+		print 'tracks count:', tracks_count
+		for block in range(tracks_count):
+			track_namber = int(fp.readline().split('\n')[0]) # tracks number
+			offset = int(fp.readline().split('\n')[0]) # frame offset
+			frame_range = int(fp.readline().split('\n')[0]) # frame range (time)
+			k, t_node = create_track() # create transform node for track
+			progIncr = 100.0 / tracks_count
+			if task.isCancelled():
+				nuke.executeInMainThread(nuke.message, args=('Aborted',))
+				return
+			task.setProgress(int(block * progIncr)) # indicator for progress bar
+			for frame in range(frame_range):
+				track = fp.readline().split(' ') # read track data number track, X, Y
+				X = float(track[1]) - t_node['center'].getValue()[0] # move X by cetner
+	            Y = float(track[2]) - t_node['center'].getValue()[1] # move Y by cetner
+	            k.setValueAt(X, int(track[0]), 0) # set keyframe for X
+	            k.setValueAt(Y, int(track[0]), 1) # set keyframe for Y
     
+threading.Thread(target=get_track_data()).start()
