1322c27e937c774cb4fc537351acf69f9afb617a
sharimo <victor.lavrentev@gmail.com>
2020-03-02 00:35:27 +0200

    Add existing file

--- Qt/src/gui_abc_list.py
+++ Qt/src/gui_abc_list.py
@@ -5,19 +5,18 @@ import alembic
 import types
 
 
-def abc_tree(i_obj, tree_list={}):
+def abc_tree(i_obj):
+    tree_dict = {}
     for index in xrange(i_obj.getNumChildren()):
         child = i_obj.getChild(index)
-        tree_list[i_obj.getName()] = child.getName()
-        abc_tree(child, tree_list)
-    return tree_list
+        tree_dict[child.getName()] = abc_tree(child)
+    return tree_dict
 
 
 def get_abc_tree(i_path):
-    i_archive = alembic.Abc.IArchive(i_path)
+    i_archive = alembic.Abc.IArchive(str(i_path))
     i_archive.getTop().getName()
     tree_dict = abc_tree(i_archive.getTop())
-    del tree_dict['ABC']
     return tree_dict
 
 
@@ -27,59 +26,53 @@ class ListAlembic(QDialog):
         super(ListAlembic, self).__init__(parent)
         # Create widgets
         self.resize(300, 500)
-        self.alembic_file = '/home/shrimo/Desktop/course/vfx_dev/alembic/abc_separation_in.abc'
-        self.setWindowTitle("Alembic list")
-        self.l1 = QLabel("Alembic object tree")
-        self.button = QPushButton("Reload")
+        self.alembic_file = '/home/shrimo/Desktop/course/vfx_dev/alembic/abc_list.abc'
+        self.setWindowTitle("Alembic tree")
+        self.l1 = QLabel(self.alembic_file.split('/')[-1])
+        self.button = QPushButton("Load alembic")
 
-        # get_abc_tree(self.alembic_file)
-
-        # create Tree View
-        self.abc_list = QTreeView()
+        self.abc_tree_view = QTreeView()
         self.model = QStandardItemModel()
-        # self.load_tree()
-        self.abc_list.setModel(self.model)
-        self._populateTree(get_abc_tree(self.alembic_file), self.model)
+        self.abc_tree_view.setModel(self.model)
+        self.set_tree_view()
+
         # Create layout and add widgets
         layout = QVBoxLayout()
         layout.addWidget(self.l1)
-        layout.addWidget(self.abc_list)
+        layout.addWidget(self.abc_tree_view)
         layout.addWidget(self.button)
 
-        # Set dialog layout
         self.setLayout(layout)
-        # Add button signal to greetings slot
         self.button.clicked.connect(self.load_abc)
 
-    def _populateTree(self, children, parent):
-        for child in sorted(children):
+    def set_tree_view(self):
+        # set Tree View
+        self.abc_tree_view.reset()
+        self.model.clear()
+        self.root = QStandardItem('ABC')
+        self.make_tree(get_abc_tree(self.alembic_file), self.root)
+        self.model.appendRow(self.root)
+        self.abc_tree_view.expandAll()
+
+    def make_tree(self, children, parent):
+        for child in children:
             child_item = QStandardItem(child)
             parent.appendRow(child_item)
             if isinstance(children, types.DictType):
-                self._populateTree(children[child], child_item)
-
-    # def load_tree(self):
-    #     abc_tree = get_abc_tree(self.alembic_file)
-    #     parent = QStandardItem('root')
-    #     for branch in abc_tree:
-    #         item = QStandardItem(branch)
-    #         item.appendRow(QStandardItem(item))
-    #     parent.appendRow(item)
-    #     self.model.appendRow(parent)
-
-    # Open alembic file
+                self.make_tree(children[child], child_item)
 
     def load_abc(self):
-        # for i in range(3):
-        fname = QFileDialog.getOpenFileName(self, 'Open *. abc file', '/home')
-        print fname
+        self.alembic_file = QFileDialog.getOpenFileName(
+            self, 'Open *. abc file', '/home/shrimo/Desktop/course/vfx_dev/alembic')[0]
+        self.l1.setText(self.alembic_file.split('/')[-1]) 
+        self.set_tree_view()
 
 
 if __name__ == '__main__':
-    # Create the Qt Application
+    # alembic_file = '/home/shrimo/Desktop/course/vfx_dev/alembic/abc_list.abc'
+    # print get_abc_tree(alembic_file)
     app = QApplication(sys.argv)
     # Create and show the ListAlembic
     list_alembic = ListAlembic()
     list_alembic.show()
-    # Run the main Qt loop
     sys.exit(app.exec_())
